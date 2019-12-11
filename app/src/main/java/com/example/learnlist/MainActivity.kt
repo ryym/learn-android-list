ef2dd578f39c7003f7ffd320282ac60f6006eb95
ryym <ryym.64@gmail.com>
2019-12-11 14:39:48 +0900

    Load spice entries dynamically

--- app/src/main/java/com/example/learnlist/MainActivity.kt
+++ app/src/main/java/com/example/learnlist/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.learnlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spices = arrayOf(
+            "胡椒",
+            "ターメリック",
+            "コリアンダー",
+            "生姜",
+            "ニンニク",
+            "サフラン"
+        )
+
+        val list = findViewById<ListView>(R.id.spices)
+        list.adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            spices
+        )
     }
 }
