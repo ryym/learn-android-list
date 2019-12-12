ba2e08a75c7d747d1accca3e2cb4af0717ec13c2
ryym <ryym.64@gmail.com>
2019-12-12 18:54:40 +0900

    Cange list choiceMode to multipleChoiceModal

--- app/src/main/java/com/example/learnlist/MainActivity.kt
+++ app/src/main/java/com/example/learnlist/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.learnlist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.*
 import androidx.core.view.children
 
@@ -24,30 +27,46 @@ class MainActivity : AppCompatActivity() {
         val list = findViewById<ListView>(R.id.spices)
         val adapter = ArrayAdapter<String>(
             this,
-            android.R.layout.simple_list_item_multiple_choice,
+            android.R.layout.simple_list_item_checked,
             spices
         )
         list.adapter = adapter
 
 
-        list.setOnItemLongClickListener { parent, view, positnion, id ->
-            val clicked = (view as TextView).text.toString()
-            Log.d("main", clicked)
-
-            // イベントを消費しない。消費するとこの次の click listener のイベントは呼ばれなくなる。
-            false
-        }
-
-        list.setOnItemClickListener { parent, view, position, id ->
-            var msg = "選択したのは: "
-            for (child in list.children) {
-                val txt = child as CheckedTextView
-                if (txt.isChecked) {
-                    msg += txt.text.toString() + ","
-                }
+        // ListView の choiceMode を multipleChoiceModal にする場合このリスナーの登録が必須。
+        // いずれかの項目を長押しすると複数選択モードになるが、リスナーが登録されていないとなんと
+        // NullPointerException が発生する。
+        list.setMultiChoiceModeListener(object: AbsListView.MultiChoiceModeListener {
+            override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                return true
             }
-            msg = msg.substring(0, msg.length - 1)
-            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
-        }
+
+            override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                return true
+            }
+
+            override fun onItemCheckedStateChanged(
+                mode: ActionMode?,
+                position: Int,
+                id: Long,
+                checked: Boolean
+            ) { /* Do nothing */ }
+
+            override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+                return true
+            }
+
+            override fun onDestroyActionMode(mode: ActionMode?) {
+                var msg = "選択したのは: "
+                for (child in list.children) {
+                    val txt = child as CheckedTextView
+                    if (txt.isChecked) {
+                        msg += txt.text.toString() + ","
+                    }
+                }
+                msg = msg.substring(0, msg.length - 1)
+                Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
